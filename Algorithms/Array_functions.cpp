35d1de06ffb83ca0fc61d7ab4d4ddd3b0d77db8b
anikosom <37804820+anikosom@users.noreply.github.com>
2018-12-18 19:34:45 +0300

    Rename Array_functions.cpp to Algorithms/Array_functions.cpp

--- Algorithms/Array_functions.cpp
+++ Algorithms/Array_functions.cpp
@@ -14,9 +14,9 @@ bool is_equal(int **array1, const size_t rows1, const size_t columns1, int **arr
 
 int fill_array(int **array, const size_t rows, const size_t columns, const int init_value) {
 	int value, elem_1 = init_value;   
-	// значение каждого элемента массива вычисляется в зависимости от его индексов в массиве
+	// Г§Г­Г Г·ГҐГ­ГЁГҐ ГЄГ Г¦Г¤Г®ГЈГ® ГЅГ«ГҐГ¬ГҐГ­ГІГ  Г¬Г Г±Г±ГЁГўГ  ГўГ»Г·ГЁГ±Г«ГїГҐГІГ±Гї Гў Г§Г ГўГЁГ±ГЁГ¬Г®Г±ГІГЁ Г®ГІ ГҐГЈГ® ГЁГ­Г¤ГҐГЄГ±Г®Гў Гў Г¬Г Г±Г±ГЁГўГҐ
 	for (size_t n = 0; n < rows; n++) {
-		(n > columns - 1) ? elem_1 += columns : elem_1 += n;   // вычисление начального значения для первого элемента строки
+		(n > columns - 1) ? elem_1 += columns : elem_1 += n;   // ГўГ»Г·ГЁГ±Г«ГҐГ­ГЁГҐ Г­Г Г·Г Г«ГјГ­Г®ГЈГ® Г§Г­Г Г·ГҐГ­ГЁГї Г¤Г«Гї ГЇГҐГ°ГўГ®ГЈГ® ГЅГ«ГҐГ¬ГҐГ­ГІГ  Г±ГІГ°Г®ГЄГЁ
 		value = elem_1;
 		for (size_t m = 0; m < columns; m++) {
 			(n + m > rows) ? value += rows : value += m + n;
@@ -30,7 +30,7 @@ int fill_array(int **array, const size_t rows, const size_t columns, const int i
 
 int print_array(int **array, const size_t rows, const size_t columns) {
 	setlocale(LC_ALL, "rus");
-	cout << "Массив:\n";
+	cout << "ГЊГ Г±Г±ГЁГў:\n";
 	for (size_t i = 0; i < rows; i++)
 	{
 		for (size_t j = 0; j < columns; j++)
