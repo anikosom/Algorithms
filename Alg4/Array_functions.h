4d19bb6f8f131b2a21ce2482b2dfc14159b1d66a
anikosom <37804820+anikosom@users.noreply.github.com>
2018-12-21 22:32:27 +0300

    Update Array_functions.h

--- Alg4/Array_functions.h
+++ Alg4/Array_functions.h
@@ -2,20 +2,20 @@
 class Array {
 private:
 	size_t size;
-	
-	
-public:
 	int *array;
 	char *char_array;
+	void MoveDown(int root, int bottom);
+	void quicksort(size_t left, size_t right);
+public:
+	
 	Array(size_t elements);
 	~Array();
 	void print();
 	void print_char();
 	int BinarySearch(int key);
-	void QuickSort(size_t left, size_t right);
+	void QuickSort();
 	void BubbleSort();
 	void HeapSort();
-	void MoveDown(int root, int bottom);
 	void BogoSort();
 	void CountingSort();
 	bool is_equal(Array *arr2);
