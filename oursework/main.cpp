88ddb51ab05f8980e5439bf04038605ab0ab9d10
anikosom <37804820+anikosom@users.noreply.github.com>
2018-12-27 14:27:45 +0300

    Rename main.cpp to oursework/main.cpp

---
+++ oursework/main.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <math.h>
+# include <stdio.h>
+# include <string.h>
+#include <vector>
+#include "Notation.h"
+using namespace std;
+
+int main() {
+	char string[100];
+	printf("Enter infix expression: ");
+	gets_s(string);
+	Notation expression(string, strlen(string));
+	expression.infixtoprefix();
+	cout << "\n\nResult: ";
+	cout << expression.Calculator();
+}
